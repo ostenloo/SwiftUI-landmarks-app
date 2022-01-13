448d74c254a61c83bcfe92ee112786245aa0a3d0
ostenloo <2ea64fad26@gmail.com>
2022-01-13 11:24:46 -0600

    fixed watch Landmarks

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -16,20 +16,16 @@ struct LandmarksApp: App {
             ContentView()
                 .environmentObject(modelData)
         }
-        .commands {
-            LandmarkCommands()
-        }
-        
         #if !os(watchOS)
         .commands {
             LandmarkCommands()
         }
         #endif
-        
+
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
         #endif
-        
+
         #if os(macOS)
         Settings {
             LandmarkSettings()
